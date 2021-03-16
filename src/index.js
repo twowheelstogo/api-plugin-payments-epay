93c1ea45c9c3cb6d81ac904313621da78a8c98dd
Luis Celano <luiscelano@twowheelstogo.com>
2021-03-16 10:52:23 -0600

    creating all needed functions to ePay plugin

--- src/index.js
+++ src/index.js
@@ -1,5 +1,26 @@
-
-
+import {EPAY_PACKAGE_NAME} from "./utils/constants";
+import i18n from "./i18n";
+import schemas from "./schemas";
+import pkg from "../package.json";
+import epayCreateAuthorizedPayment from "./utils/createAuthorizedPayment";
+import epayCapturePayment from "./utils/capturePayment";
 export default async function register(app){
-    
+    await app.registerPlugin({
+        label:"ePay Payments",
+        name:EPAY_PACKAGE_NAME,
+        version:pkg.version,
+        i18n,
+        graphQL:{
+            schemas
+        },
+        paymentMethods:{
+            name:"epay_payment",
+            canRefund:true,
+            displayName:"EPay Payment",
+            functions:{
+                createAuthorizedPayment:epayCreateAuthorizedPayment,
+                createPayment:epayCapturePayment
+            }
+        }
+    })
 }
---
+++ src/utils/constants.js
@@ -0,0 +1,2 @@
+export const EPAY_PACKAGE_NAME = "payments_epay";
+export const SERVICE_DOMAIN = "https://epaytestvisanet.com.gt";
