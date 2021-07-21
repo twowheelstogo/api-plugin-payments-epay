50b00df1a64b558423971f8ed4ebba6645773b8e
diego jose orellana bojorquez <diegoorellana@twowheelstogo.com>
2021-07-21 10:51:21 -0600

    fix fetch decode json

--- src/services/epay.service.js
+++ src/services/epay.service.js
@@ -4,7 +4,11 @@ export default async (body, action = 1) => {
   const invoiceUrl = process.env.INVOICE_URL;
   let data = await fetch(
     `${invoiceUrl}/api/epay?action=${action}`,
-    { method: "POST", body }
+    {
+      method: "POST",
+      body: JSON.stringify(body),
+      headers: { "Content-Type": "application/json" }
+    }
   );
   console.log("data es", data);
   data = await data.json();
