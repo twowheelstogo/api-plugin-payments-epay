c3ca56a4ede92d75a61141b15ef511166bb6a982
luiscelanomrb <luiscelano@twowheelstogo.com>
2021-07-26 17:23:06 -0600

    Epay model udpated

--- src/models/epay.model.js
+++ src/models/epay.model.js
@@ -1,5 +1,5 @@
 export default (shopperIP, auditNumber = "", cardNumber = "", cardExpiry = "", amount = 0, cvv2 = "") => {
-  let pan = cardNumber && cardNumber.replace(" ","");
+  let pan = cardNumber && cardNumber.replace(/ /g,"");
   let expvalues = cardExpiry && cardExpiry.split("/");
   let expdate =  `${expvalues[1]}${expvalues[0]}`
   const _epayModel = { shopperIP, pan, expdate, amount, cvv2, auditNumber };
