f58d46210e4e71b9d3728c427c33e889ffbb4799
djorellanabmrb <70344779+djorellanabmrb@users.noreply.github.com>
2021-08-03 14:03:23 -0600

    add model email

--- src/models/epay.model.js
+++ src/models/epay.model.js
@@ -1,8 +1,8 @@
-export default (shopperIP, auditNumber = "", cardNumber = "", cardExpiry = "", amount = 0, cvv2 = "") => {
+export default (shopperIP, auditNumber = "", email="", cardNumber = "", cardExpiry = "", amount = 0, cvv2 = "") => {
   let pan = cardNumber && cardNumber.replace(/ /g,"");
   let expvalues = cardExpiry && cardExpiry.split("/");
   let expdate =  `${expvalues[1]}${expvalues[0]}`
-  const _epayModel = { shopperIP, pan, expdate, amount, cvv2, auditNumber };
+  const _epayModel = { shopperIP, pan, expdate, amount, cvv2, auditNumber, email };
   console.log("Epay_model", _epayModel);
   return _epayModel;
 };
