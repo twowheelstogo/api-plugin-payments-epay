490f19f8968ecc20b87df7d6b393210844706bec
djorellanabmrb <70344779+djorellanabmrb@users.noreply.github.com>
2021-08-06 11:45:58 -0600

    card name on model
    add name of the card

--- src/models/epay.model.js
+++ src/models/epay.model.js
@@ -1,8 +1,7 @@
-export default (shopperIP, auditNumber = "", email="", cardNumber = "", cardExpiry = "", amount = 0, cvv2 = "") => {
+export default (shopperIP, auditNumber = "", email="", cardNumber = "", cardExpiry = "", amount = 0, cvv2 = "", cardName = "") => {
   let pan = cardNumber && cardNumber.replace(/ /g,"");
   let expvalues = cardExpiry && cardExpiry.split("/");
-  let expdate =  `${expvalues[1]}${expvalues[0]}`
-  const _epayModel = { shopperIP, pan, expdate, amount, cvv2, auditNumber, email };
-  console.log("Epay_model", _epayModel);
+  let expdate =  `${expvalues[1]}${expvalues[0]}`;
+  const _epayModel = { shopperIP, pan, expdate, amount, cvv2, auditNumber, email, cardName };
   return _epayModel;
 };
