9fda1a8c940626c268e7526a0044fbf50012b9b5
luiscelanomrb <luiscelano@twowheelstogo.com>
2021-08-11 16:29:24 -0600

    making billingAddress optional

--- src/utils/createAuthorizedPayment.js
+++ src/utils/createAuthorizedPayment.js
@@ -30,11 +30,13 @@ export default async function exampleCreateAuthorizedPayment(context, input) {
       cardName
     }
   } = input;
+  console.log("billingAddress",billingAddress)
+  console.log('paymentData',input.paymentData);
   const model = EpayModel("190.56.108.46", "0", email, cardNumber, cardExpiry, amount, cardCVV, cardName);
   const res = await EpayService(model, 1);
   return {
     _id: Random.id(),
-    address: billingAddress,
+    address: billingAddress || null,
     amount,
     createdAt: new Date(),
     data: {
