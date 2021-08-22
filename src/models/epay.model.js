fa7ad4903cd6da041f44e117271054786010f2bd
djorellanabmrb <70344779+djorellanabmrb@users.noreply.github.com>
2021-08-22 08:38:12 -0600

    Update epay.model.js

--- src/models/epay.model.js
+++ src/models/epay.model.js
@@ -1,8 +1,9 @@
 export default (shopperIP, auditNumber = "", email="", cardNumber = "", cardExpiry = "", amount = 0, cvv2 = "", cardName = "") => {
   let pan = cardNumber && cardNumber.replace(/ /g,"");
+  console.log(cardExpiry);
   let expvalues = cardExpiry && cardExpiry.split("/");
   let expdate =  `${expvalues[1]}${expvalues[0]}`;
-  
+  console.log(expdate);
   let metaPan = pan.replace( /\d(?=\d{4})/gm, 'x');
   const _epayModel = { shopperIP, pan, expdate, amount, cvv2, auditNumber, email, cardName, metadata:{
     pan: metaPan,
